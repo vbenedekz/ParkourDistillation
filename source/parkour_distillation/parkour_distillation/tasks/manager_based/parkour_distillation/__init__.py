5efda4896a0a65389a5a3dead4534324445ba0dd
vbenedekz <vas.benedek@gmail.com>
2025-11-13 20:34:14 +0100

    feat: add Go2 base files

--- source/parkour_distillation/parkour_distillation/tasks/manager_based/parkour_distillation/__init__.py
+++ source/parkour_distillation/parkour_distillation/tasks/manager_based/parkour_distillation/__init__.py
@@ -13,7 +13,7 @@ from . import agents
 
 
 gym.register(
-    id="Template-Parkour-Distillation-v0",
+    id="vbz-Template-Parkour-Distillation-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
